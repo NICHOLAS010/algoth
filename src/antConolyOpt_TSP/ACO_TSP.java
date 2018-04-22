ddb948f7148defe592b686d6b971fc7917a1ed15
puming <jammyzhu123@outlook.com>
2018-04-23 00:07:09 +0800

    增加了 ACO_TSP 的初始化部分

--- src/antConolyOpt_TSP/ACO_TSP.java
+++ src/antConolyOpt_TSP/ACO_TSP.java
@@ -9,6 +9,12 @@ import java.util.regex.Pattern;
 
 public class ACO_TSP {
 
+    private int iterNum;
+
+    public ACO_TSP(int iterNum){
+        this.iterNum;
+    }
+
     public void initTSP(){
         String tsp_file_path = System.getProperty("user.dir") + "/src/antConolyOpt_TSP/att48.tsp";
         File f = new File(tsp_file_path);
@@ -29,17 +35,28 @@ public class ACO_TSP {
         String[] tmp2 = context.split("\n");
         int[][] tsp_info = new int[48][2];
         int cnt = 0;
-        for(int i=0;i<tmp2.length-1;i++){
+        for(int i=0;i<tmp2.length;i++){
             Matcher m = r.matcher(tmp2[i]);
             if(m.find()){
-                tsp_info[cnt] = this.string2intArray(m.group(0).substring(2));
+                tsp_info[cnt] = this.string2intArray(m.group(0));
+                cnt ++;
             }
         }
-        System.out.println(tsp_info);
+
+        int[][] tsp_distance = new int[tsp_info.length][tsp_info.length];
+        for(int i=0;i<tsp_info.length;i++){
+            for(int j=0;j<tsp_info.length;j++){
+                tsp_distance[i][j] = (int)distanceCal(tsp_info[i],tsp_info[j]);
+            }
+        }
+
+        System.out.println(tsp_distance);
+
     }
 
 
     public void doIter(){
+
 
     }
 
@@ -47,8 +64,16 @@ public class ACO_TSP {
 
     }
 
-    public void greedyCal(){
+    public void greedyPath(){
 
+
+    }
+
+    public double distanceCal(int[] p1, int[] p2){
+        double tp1 = Math.pow(p1[0]-p2[0],2);
+        double tp2 = Math.pow(p1[1]-p2[1],2);
+        double tp =  Math.sqrt(tp1+tp2);
+        return tp;
     }
 
     public static void main(String[] args) {
@@ -58,9 +83,9 @@ public class ACO_TSP {
 
     public int[] string2intArray(String strin){
         String[] tpStr = strin.split(" ");
-        int[] tmp = new int[tpStr.length];
+        int[] tmp = new int[tpStr.length-1];
         for(int i=0;i<tmp.length;i++){
-            tmp[i] = Integer.valueOf(tpStr[i]);
+            tmp[i] = Integer.valueOf(tpStr[i+1]);
         }
         return tmp;
     }
